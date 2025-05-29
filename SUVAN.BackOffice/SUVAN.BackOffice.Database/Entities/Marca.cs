6855e3ca8ffd0b64f64721e0dac04aefeab069b7
Jordan Jimenez <jordan.jimenez@kratosconsultores.com.mx>
2025-05-28 23:15:25 -0600

    Actualización de Tablas en Entity

--- SUVAN.BackOffice/SUVAN.BackOffice.Database/Entities/Marca.cs
+++ SUVAN.BackOffice/SUVAN.BackOffice.Database/Entities/Marca.cs
@@ -5,7 +5,7 @@ namespace SUVAN.BackOffice.Database.Entities;
 
 public partial class Marca
 {
-    public short CMarca { get; set; }
+    public short IdMarca { get; set; }
 
     public string? Descrip { get; set; }
 }
--- SUVAN.BackOffice/SUVAN.BackOffice.Database/Entities/Zona.cs
+++ SUVAN.BackOffice/SUVAN.BackOffice.Database/Entities/Zona.cs
@@ -9,7 +9,25 @@ public partial class Zona
 
     public string NombreZona { get; set; } = null!;
 
+    public string? Rfc { get; set; }
+
+    public string? Domicilio { get; set; }
+
+    public string? Telefono1 { get; set; }
+
+    public string? Telefono2 { get; set; }
+
+    public string? Responsable { get; set; }
+
+    public DateTime? FechaApertura { get; set; }
+
+    public int? IdEmpresa { get; set; }
+
+    public ulong? Activo { get; set; }
+
     public virtual ICollection<Depositosdisponible> Depositosdisponibles { get; set; } = new List<Depositosdisponible>();
+
+    public virtual Empresa? IdEmpresaNavigation { get; set; }
 
     public virtual ICollection<Taller> Tallers { get; set; } = new List<Taller>();
 }
